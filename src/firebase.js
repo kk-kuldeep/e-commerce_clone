0eaa5303e87283e8feaed655dc9f2f1492a3240c
kk-kuldeep <ks13bro@gmail.com>
2022-08-31 12:13:55 +0530

    change firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcPSKlYtpdzBoAC8soeSmIARMzVKzrf5I",
-  authDomain: "challenge-4b2b2.firebaseapp.com",
-  databaseURL: "https://challenge-4b2b2.firebaseio.com",
-  projectId: "challenge-4b2b2",
-  storageBucket: "challenge-4b2b2.appspot.com",
-  messagingSenderId: "962418448875",
-  appId: "1:962418448875:web:f6cce5eeaf819481f661ae",
+  apiKey: "AIzaSyC4YKDDj6GA-OEwfW3mnE5Fh_um1ExUVcg",
+  authDomain: "clone-b64f8.firebaseapp.com",
+  projectId: "clone-b64f8",
+  storageBucket: "clone-b64f8.appspot.com",
+  messagingSenderId: "113737320336",
+  appId: "1:113737320336:web:addcc9dc44417bbcee5515",
+  measurementId: "G-35Q3XVSTC8"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
